b4b74c495730567e71cfc3fc02f5bdb11907beb9
Ali Umar <ali.umar.work@gmail.com>
2023-11-13 15:15:40 +0000

    update server network code

--- server/src/main/java/server/RequestHandler.java
+++ server/src/main/java/server/RequestHandler.java
@@ -10,43 +10,42 @@ import com.sun.net.httpserver.HttpServer;
 import utils.Utility;
 
 public class RequestHandler {
-	public RequestHandler() { } 
+	public RequestHandler() {
+	}
 
 	public static class OrderHandler implements HttpHandler {
 
 		public void handle(HttpExchange exchange) throws IOException {
 			System.out.println(exchange.toString());
 			System.out.println("test 1");
-			Map<String, String> parms = Utility.queryToMap(exchange.getRequestURI().getQuery());
-//			String response = "Hello World! " + "P1 was: " + parms.get("p1") + " and p2 was: " + parms.get("p2");
-			String response = "Hello, We are in test 1";
+			System.out.println("Ali Umar");
+
+			String response = "This should return handle ordering";
+
+			System.out.println("Ali" + response);
 			exchange.sendResponseHeaders(200, response.length());
+
 			OutputStream os = exchange.getResponseBody();
+
 			os.write(response.getBytes());
-			try {
-				wait(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			response = " FOO";
-			exchange.sendResponseHeaders(200, response.length());
-			OutputStream os2 = exchange.getResponseBody();
-			os2 = exchange.getResponseBody();
-			os2.write(response.getBytes());
 			os.close();
-			os2.close();
 		}
 	}
 
 	public static class ProductHandler implements HttpHandler {
 
 		public void handle(HttpExchange exchange) throws IOException {
+			System.out.println(exchange.toString());
 			System.out.println("test 2");
-//			Map<String, String> parms = queryToMap(exchange.getRequestURI().getQuery());
-			String response = "Hello, We are in test 2";
+			System.out.println("Ali Umar");
+
+			String response = "This should return a list products";
+
+			System.out.println("Ali" + response);
 			exchange.sendResponseHeaders(200, response.length());
+
 			OutputStream os = exchange.getResponseBody();
+
 			os.write(response.getBytes());
 			os.close();
 		}
