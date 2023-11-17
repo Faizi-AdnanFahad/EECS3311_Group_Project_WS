e609e60f14144e135de36aa72a32d925cec14e09
Shreya Vaghani <shreyavaghani6123@gmail.com>
2023-11-17 18:14:47 -0500

    Needs review

--- server/src/main/java/database/AdminDAO.java
+++ server/src/main/java/database/AdminDAO.java
@@ -1,5 +1,75 @@
 package database;
+import java.sql.*;
+
+
 
 public class AdminDAO {
+	
+	//This method retrieves the password of the respective username by the user in the GUI.
+    public static String retrivePassword(String username) {
+  	  
+  	  String path = "jdbc:sqlite:C://dev//repos//EECS3311_Group_Project_WS//database//admin.db";
+ 		String query = "SELECT Password FROM credentials WHERE Username = ?;";
+ 		String password = null;
+ 		
+ 		
+ 		try {
+ 		Class.forName("org.sqlite.JDBC");
+		Connection conn = DriverManager.getConnection(path);
+		PreparedStatement pstmt = conn.prepareStatement(query);
+		
+		pstmt.setString(1,username);
+      ResultSet  resultSet = pstmt.executeQuery();
+      
+      if(resultSet.next()) {
+      	password = resultSet.getString("Password");
+      }
+ 		}
+      catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 		
+     
+ 		return password;
 
 }
+    
+    
+   //This method is to check if the User exists in the system and is stored in the database or no. 
+    public  static boolean checkIfUserExists(String username) {
+    	
+    	String path = "jdbc:sqlite:C://dev//repos//EECS3311_Group_Project_WS//database//admin.db";
+ 		String query = "SELECT COUNT(*) as count FROM credentials WHERE Username = ?;";
+ 		int count = 0;
+ 		
+ 		
+ 		try {
+ 	 		Class.forName("org.sqlite.JDBC");
+ 			Connection conn = DriverManager.getConnection(path);
+ 			PreparedStatement pstmt = conn.prepareStatement(query);
+ 			
+ 			pstmt.setString(1,username);
+ 	      ResultSet  resultSet = pstmt.executeQuery();
+ 	      
+ 	      //Get the count of rows with the input username 
+ 	      if(resultSet.next()) {
+ 	    	  count = resultSet.getInt("count");
+ 	      }
+ 		}
+ 	     catch (SQLException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		} catch (ClassNotFoundException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		} 	
+ 		
+ 		return count>0;
+ 		
+    }
+    
+}
+	
--- server/src/main/java/database/ProductDAO.java
+++ server/src/main/java/database/ProductDAO.java
@@ -1,5 +1,78 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ProductDAO {
 
+	//This method retrieves the price of the respective Product from the user
+	public static int retrivePrice(String productName) {
+
+		String path = "jdbc:sqlite:C://dev//repos//EECS3311_Group_Project_WS//database//product.db";
+		String query = "SELECT Price FROM product WHERE Name = ?;";
+		int price = 0;
+
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection(path);
+			PreparedStatement pstmt = conn.prepareStatement(query);
+
+			pstmt.setString(1,productName);
+			ResultSet  resultSet = pstmt.executeQuery();
+
+			if(resultSet.next()) {
+				price = resultSet.getInt("Price");
+			}
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 		
+
+		return price;
+	}
+
+	//This method retrieves the StockQuantity of the respective Product from the user
+	public static int retriveStockQuantity(String productName) {
+
+		String path = "jdbc:sqlite:C://dev//repos//EECS3311_Group_Project_WS//database//product.db";
+		String query = "SELECT StockQuantity FROM product WHERE Name = ?;";
+		int stockQuantity = 0;
+
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection(path);
+			PreparedStatement pstmt = conn.prepareStatement(query);
+
+			pstmt.setString(1,productName);
+			ResultSet  resultSet = pstmt.executeQuery();
+
+			if(resultSet.next()) {
+				stockQuantity = resultSet.getInt("StockQuantity");
+			}
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 		
+
+		return stockQuantity;
+
+	}
+
+
+
 }
+
