8dc64bf97f95adb1bae2409335ebf499d9dc567a
Jansiz <ali.jansizzz@gmail.com>
2023-12-05 00:21:12 -0500

    added server gui messages

--- server/src/main/java/model/orderstate/IOrderState.java
+++ server/src/main/java/model/orderstate/IOrderState.java
@@ -5,6 +5,9 @@ import model.Order;
 public interface IOrderState {
 
 	void processOrder(Order order);
+	
+	
+	public void sendMessage(Order order);
 }
 
 
--- server/src/main/java/model/orderstate/OrderedQntySMEqualToAvailableQntyState.java
+++ server/src/main/java/model/orderstate/OrderedQntySMEqualToAvailableQntyState.java
@@ -1,24 +1,40 @@
 package model.orderstate;
 
+import gui.ServerGUI;
 import model.Order;
+
+import util.Messages;
 
 public class OrderedQntySMEqualToAvailableQntyState implements IOrderState {
 
 	public void processOrder(Order order) {
-		System.out.println("------------------------------");
-		String message = String.format("Order is finalized for Product %s and Quantity %d with total price %.2f.",
-				order.getOrderedProduct().getName(), order.getOrderedQuantity(), order.getOrderPrice());
-		System.out.println(message);
-		System.out.println("------------------------------");
-		
-		// Update all viewers for Observer pattern
-		/*********************Observer pattern************************/
 		System.out.println("/*********************Observer pattern************************/");
 		order.addViewers();
 		order.updateViewers();
 		System.out.println("/*********************Observer pattern************************/");
 		/*************************************************************/
 
+		
+		
+		sendMessage(order);
+	}
+
+	public void sendMessage(Order order) {
+		
+		ServerGUI serverGUI = ServerGUI.getInstance();
+		
+		System.out.println("------------------------------");
+		String message = String.format(Messages.MSG_ORDER_COMPLETED,
+				order.getOrderedProduct().getName(), order.getOrderedQuantity(), order.getOrderPrice());
+		System.out.println(message);
+		System.out.println("------------------------------");
+		
+		// Update all viewers for Observer pattern
+		/*********************Observer pattern************************/
+		
+		serverGUI.stateMessage(message);
+		
+		
 	}
 
 }
